6485d5d777afda0b5d3bfa47d98d31a0d345b07c
venkat ponnapalli <getvenkathere@gmail.com>
2021-02-09 16:47:58 +0530

    this is our employee poc

---
+++ employee-frontend/build/precache-manifest.59c56e7c98e17a2fecec7c2fb3e60d25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2094eec9c901a9582fd6344cfcaa89bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20c2d7eb6f4c41958350",
+    "url": "/static/css/main.225fe4ae.chunk.css"
+  },
+  {
+    "revision": "0ba588c8cd6b64fc82eb",
+    "url": "/static/js/2.89818435.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.89818435.chunk.js.LICENSE"
+  },
+  {
+    "revision": "20c2d7eb6f4c41958350",
+    "url": "/static/js/main.9304412c.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  },
+  {
+    "revision": "53dd4635703c5e3fe9bb657bc6122de9",
+    "url": "/static/media/logo.53dd4635.png"
+  }
+]);
